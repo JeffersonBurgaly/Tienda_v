37d7dcaf906afc13e3d0a0561d5676693087421d
Administrator <Administrator@AztroOS-PC>
2023-02-08 20:52:01 -0600

    Se crean repositorios y clases Pais y Persona

---
+++ src/main/java/com/tienda/repository/PersonaRepository.java
@@ -0,0 +1,7 @@
+package com.tienda.repository;
+import com.tienda.entity.Persona;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonaRepository extends CrudRepository<Persona,Long>{
+    
+}
