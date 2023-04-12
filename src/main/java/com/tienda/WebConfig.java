004f8b7eb9de5351f301b480bde7ea27d5fdc5a0
Administrator <Administrator@AztroOS-PC>
2023-04-12 16:15:46 -0600

    Se crea el cambio de lenguaje y se anade el footer y nav de la template en el crear

---
+++ src/main/java/com/tienda/WebConfig.java
@@ -0,0 +1,29 @@
+package com.tienda;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+public class WebConfig implements WebMvcConfigurer{
+    @Bean
+    public SessionLocaleResolver localeResolver(){
+      var slr = new SessionLocaleResolver();
+      slr.setDefaultLocale(new Locale("es"));
+      return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    
+    @Override 
+    public void addInterceptors (InterceptorRegistry registro){
+       registro.addInterceptor (localeChangeInterceptor()); 
+    }
+}
